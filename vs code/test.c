7f174c67acd83943fcddcc54ac40e53366400bb1
Jacob1048596 <qinsheng2580@foxmail.com>
2020-03-19 17:37:41 +0800

    vscode调试

---
+++ vs code/test.c
@@ -0,0 +1,20 @@
+// #include<stdio>
+// int main()
+// {
+//     printf("Hello World!");
+//     return 0;
+// }
+// #include<stdio.h>
+// int main()
+// {
+//     printf("Hello C!\nI like programing");
+//     return 0;
+// }
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    scanf("%d %d",&a,&b);
+    printf("%d*%d=%d",a,b,a*b);
+    return 0;
+}
