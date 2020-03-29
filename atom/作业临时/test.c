694c2bb00b725a970d950df660370dd220748c74
Jacob1048596 <qinsheng2580@foxmail.com>
2020-03-29 21:15:17 +0800

    FXXK SCANF

--- atom/作业临时/test.c
+++ atom/作业临时/test.c
@@ -10,31 +10,49 @@
 #include<stdlib.h>
 #include<string.h>
 #include<assert.h>
+#include <math.h>
+double power(double n,int k)
+{
+  double result;
+     if(k==0) return 1;
+  else if(k>0)
+   {
+     result= (n*power(n,k-1));
+     return result;
+   }
+     else if(k<0)
+     {
+       k=k*-1;
+       result= (n*power(n,k-1));
+       double ret =1/result;
+       return ret;
+     }
+}
 int main()
 {
-	int arr[100] = { 0 };
-	int n = 0;
-	printf("请输入元素个数:\n");
-	scanf("%d", &n);
-	for (int k = 0; k < n; k++)
-	{
-		scanf("%d", &arr[k]);
-	}
-  int num[50]={0};
-  for(int i=0;i<100;i++)
-  {
-    num[arr[i]]++;
-  }
-  for(int i=0;i<50;i++)
-  {
-    if(num[i]!=0)
-    printf("[%d] %d ",i,num[i]);
-  }
-  printf("\n");
-  for(int i=1;i<50;i++)
-  {
-    if(num[i]==1) printf("%d ",i);
-  }
+	// int arr[100] = { 0 };
+	// int n = 0;
+	// printf("请输入元素个数:\n");
+	// scanf("%d", &n);
+	// for (int k = 0; k < n; k++)
+	// {
+	// 	scanf("%d", &arr[k]);
+	// }
+  // int num[50]={0};
+  // for(int i=0;i<100;i++)
+  // {
+  //   num[arr[i]]++;
+  // }
+  // for(int i=0;i<50;i++)
+  // {
+  //   if(num[i]!=0)
+  //   printf("[%d] %d ",i,num[i]);
+  // }
+  // printf("\n");
+  // for(int i=1;i<50;i++)
+  // {
+  //   if(num[i]==1) printf("%d ",i);
+  // }
 	// for (int i = n-1; i >=0; i--)
 	// { int count = 0;
 	// 	for (int j = 0; i < n; j++)
@@ -51,6 +69,13 @@ int main()
 	// 	}
 	// 	continue;
 	// }
+	int money,year;
+	double rate;
+
+	scanf("%d %d %lf",&money,&year,&rate);
+	printf("%d,%d,%f\n",money,year,rate );
+	double sum=money*power((1+rate),year);
+	printf("%7.2f\n",sum);
 	return 0;
 	system("pause");
 }
