b9900b493e954a54185fcbb8b6910291188daab1
Jacob1048596 <qinsheng2580@foxmail.com>
2020-04-04 23:35:53 +0800

    .

--- atom/字符串比较.c
+++ atom/字符串比较.c
@@ -10,11 +10,31 @@ int mystrcmp(char* a,char* b)
   }
   return 0;
 }
+void test()
+{
+  char* arry[5]=
+  {
+    "hello",
+  "TheWorld",
+  "wryyyyyyy",
+  "KonoDioDa!",
+  "RoadRoller"
+  };
+  printf("%s\n",*arry);
+  printf("%s\n",arry[0]);
+  printf("%c\n",arry[0][1]);
+  printf("%s\n",*arry);
+  printf("%s\n",*arry+0);
+  //printf("%c\n",**arry);
+  printf("%s\n",*(arry+1));
+
+}
 int main(int argc, char const *argv[]) {
-  char* a="hh";
-  char* b="hh";
-  int p=mystrcmp(a,b);
-  printf("%d\n",p);
-  printf(a);
+  // char* a="hh";
+  // char* b="hh";
+  // int p=mystrcmp(a,b);
+  // printf("%d\n",p);
+  // printf(a);
+  test();
   return 0;
 }
