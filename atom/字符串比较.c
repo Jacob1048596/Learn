2c408a744f269b7041360dce2cb4ef660a7af095
Jacob1048596 <qinsheng2580@foxmail.com>
2020-04-04 21:05:33 +0800

    .

--- atom/字符串比较.c
+++ atom/字符串比较.c
@@ -2,16 +2,17 @@
 #include <string.h>
 int mystrcmp(char* a,char* b)
 {
-  while (a||b) {
+  while (*a!='\0'||*b!='\0') {
     /* code */
     if(*a>*b) return 1;
     else if(*a<*b) return -1;
     else {a++;b++;}
   }
+  return 0;
 }
 int main(int argc, char const *argv[]) {
-  char* a="hello";
-  char* b="world";
+  char* a="hh";
+  char* b="hh";
   int p=mystrcmp(a,b);
   printf("%d\n",p);
   return 0;
