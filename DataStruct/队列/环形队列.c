7c240c57c669ca16ec8bf9ea8418248b2af17b23
Jacob1048596 <qinsheng2580@foxmail.com>
2020-04-08 21:05:23 +0800

    4.8

--- DataStruct/队列/环形队列.c
+++ DataStruct/队列/环形队列.c
@@ -3,5 +3,10 @@
 
 int main(int argc, char const *argv[]) {
   /* code */
+  printf("%d\n",(-1&7) );
+  printf("%d\n",(0&7) );
+  int x=10;
+  printf("%d\n",~x );
+  printf("%d\n",x^-1 );
   return 0;
 }
