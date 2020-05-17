66be9e4abeae241286ce395ce3dac35ea0cde0a2
Jacob1048596 <qinsheng2580@foxmail.com>
2020-05-17 16:53:53 +0800

    5.16
    就当16了

--- C++/learn.cpp
+++ C++/learn.cpp
@@ -1,9 +1,31 @@
 #include<iostream>
 #include<stdio.h>
+namespace N
+{
+    int test=233;
+   long long int beast=1145141919810;
+   void print(int a)
+   {
+       printf("%d \n",a);
+   }
+   
+}
 using namespace std;
+int add(int a,int b)
+{
+    return a+b;
+}
+float add(float a,float b)
+{
+    return a+b;
+}
 int main()
 {
     cout<<"C++ language"<<"?"<<endl;
-    printf("C language");
+    printf("C language\n");
+    N::print(114514);
+    N::print(N::test);
+    cout<<N::beast<<endl; 
+   //printf("%s",*N::beast);
     return 0;
 }
--- vs code/test.c
+++ vs code/test.c
@@ -10,11 +10,46 @@
 //     printf("Hello C!\nI like programing");
 //     return 0;
 // }
+// #include<stdio.h>
+// int main()
+// {
+//     int a,b;
+//     scanf("%d %d",&a,&b);
+//     printf("%d*%d=%d",a,b,a*b);
+//     return 0;
+// }
 #include<stdio.h>
+#include<assert.h>
+#define MAXSIZE 5000
+
 int main()
 {
-    int a,b;
-    scanf("%d %d",&a,&b);
-    printf("%d*%d=%d",a,b,a*b);
+    char* str;
+    getchar();
+    putchar(str);
+    
+//     char str[MAXSIZE];
+//     scanf("%s",&str);a
+//    int word[256]={0};
+//     char* ptr=str;
+//     int i=0;
+//     while(str[i]!='\n')
+//     {
+//         word[str[i]]++;
+//         i++;
+//     }
+// for(int i=0;i<256;i++)
+// {
+//     printf("%d ",word[i]);
+// }
+    // char newstr[MAXSIZE];
+    // for(int i=0;i<26;i++)
+    // {
+    //     for(int j=0;j<word[i];j++)
+    //     {
+    //         newstr[j]=word[i]+'a';
+    //     }
+    // }
+    //printf("%s",newstr);
     return 0;
 }
