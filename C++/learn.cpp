2385df72d818731f4f9d790dcd34252f151aa6fe
Jacob1048596 <qinsheng2580@foxmail.com>
2020-05-23 23:22:01 +0800

    5.23
    114514

--- C++/learn.cpp
+++ C++/learn.cpp
@@ -36,18 +36,67 @@ public:
     
 };
 
+class Date
+{
+private:
+    int _year;
+    int _month;
+    int _date;
+public:
+    Date(int y,int m,int d);
+    ~Date();
+    Date DateCopy(Date& d)
+    {
+        _year=d._year;
+        _month=d._month;
+        _date=d._date;
+    }
+    void PrintD()
+    {
+        cout<<_year<<" "<<_month<<" "<<_date<<endl;
+    }
+//     ostream& operator<<(ostream& _cout,const Date& d)
+// {
+//     _cout<<d._year<<" "<<d._month<<" "<<d._date<<endl;
+// }
+    Date operator+(int day)
+    {
+        _date+=day;
+        return *this;
+    }
+};
+
+Date::Date(int y=1,int m=1,int d=1)
+{
+    _year=y;
+    _month=m;
+    _date=d;
+}
+// ostream& operator<<(ostream& _cout,const Date& d)
+// {
+//     _cout<<d._year<<" "<<d._month<<" "<<d._date<<endl;
+// }
+Date::~Date()
+{
+    cout<<"~Date()"<<endl;
+}
+
 int main()
 {
-    cout<<"C++ language"<<"?"<<endl;
-    printf("C language\n");
-    N::print(114514);
-    N::print(N::test);
-    cout<<N::beast<<endl; 
-    test a;
-    a.SetData(10);
-    a.PrintData();
-   //printf("%s",*N::beast);
-   cout<<sizeof(test)<<endl;
-   cout<<"测试"<<endl;
+//     cout<<"C++ language"<<"?"<<endl;
+//     printf("C language\n");
+//     N::print(114514);
+//     N::print(N::test);
+//     cout<<N::beast<<endl; 
+//     test a;
+//     a.SetData(10);
+//     a.PrintData();
+//    //printf("%s",*N::beast);
+//    cout<<sizeof(test)<<endl;
+//    cout<<"测试"<<endl;
+    Date D1(2000,12,25);
+    D1.PrintD();
+    Date D2=D1+5;
+    D2.PrintD();
     return 0;
 }
