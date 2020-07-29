7924bfbfefd8f75f94c4afcc8ee0c2ab3da7fbc1
Jacob1048596 <qinsheng2580@foxmail.com>
2020-07-29 23:51:57 +0800

    27

--- C++/每日一题/day26.cpp
+++ C++/每日一题/day26.cpp
@@ -4,3 +4,15 @@ int main()
 {
    cout<<"今日测试成功"<<endl;
 }
+// #include <iostream>
+// #include <windows.h>
+// using namespace std;
+// int main(){
+//     cout<<"编码字符"<<endl;
+//     cout<<"CodePage:"<<GetConsoleOutputCP()<<endl;
+//     SetConsoleOutputCP(65001);
+//     cout<<"编码字符"<<endl;
+//     cout<<"CodePage:"<<GetConsoleOutputCP()<<endl;
+//     system("pause");
+//     return 0;
+// }
---
+++ C++/每日一题/tempCodeRunnerFile.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   cout<<"今日测试成功"<<endl;
+}
