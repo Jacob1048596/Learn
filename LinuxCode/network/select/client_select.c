0366ca1ec5b28a4e4d19ba64b7c39e9efc7d4344
Jacob1048596 <qinsheng2580@foxmail.com>
2020-11-21 19:09:45 +0800

    select

---
+++ LinuxCode/network/select/client_select.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<string.h>
+#include<errno.h>
+#include<pthread.h>
+
+int main()
+{
+    int cfd=socket(AF_INET,SOCK_STREAM,0);
+    struct sockaddr_in addr;
+    addr.sin_family=AF_INET;
+    addr.sin_port=htonl(2580);
+    inet_aton("192.168.1.20",&addr.sin_addr);
+    int r=connect(cfd,(struct sockaddr*)&addr,sizeof(addr));
+     if(r==-1) {perror("connect"),exit(1);}
+    printf("连接成功\n");
+
+    int key_fd=fileno(stdin);
+    int maxfd=cfd>key_fd?cfd:key_fd;
+
+    fd_set rset;
+    FD_ZERO(&rset);
+
+    char buf[1024]={};
+    for(;;)
+    {
+        FD_SET(cfd,&rset);
+        FD_SET(key_fd,&rset);
+        int ready=select(maxfd+1,&rset,NULL,NULL,NULL);
+        if(ready<=0) continue;
+        if(FD_ISSET(cfd,&rset))
+        {
+            memset(buf,0x00,sizeof(buf));
+            int r=read(cfd,buf,1024);
+            if(r<=0)
+            {
+                printf("sever close\n");
+                break;
+            }
+        }
+        if(FD_ISSET(key_fd,&rset))
+        {
+            memset(buf,0x00,sizeof(buf));
+            if(fgets(buf,1024,stdin)==NULL)
+            {
+                break;
+            }
+            write(cfd,buf,strlen(buf));
+        }
+    }
+}
---
+++ LinuxCode/network/select/svr_select.c
@@ -0,0 +1,90 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<string.h>
+#include<errno.h>
+#include<pthread.h>
+
+int tcp_init()
+{
+    int fd=socket(AF_INET,SOCK_STREAM,0);
+    int op=1;
+    setsockopt(fd,SOL_SOCKET,SO_REUSEADDR,&op,sizeof(op));
+
+    struct sockaddr_in addr;
+    addr.sin_family=AF_INET;
+    addr.sin_port=htons(2580);
+    //addr.sin_addr.s_addr=htonl(INADDR_ANY);
+    inet_aton("192.168.1.20",&addr.sin_addr);
+    int r=bind(fd,(struct sockaddr*)&addr,sizeof(addr));
+    if(r==-1) perror("bind"),exit(1);
+     printf("绑定成功\n");
+    if( (r=listen(fd,10))==-1)
+        perror("listen"),exit(1);
+    
+    return fd;
+}
+
+int main()
+{
+    int lfd=tcp_init();
+    int clients[FD_SETSIZE];
+    fd_set rset,allset;
+    int maxfd,i;
+    for(i=0;i<FD_SETSIZE;i++)
+     clients[i]=-1;
+     FD_ZERO(&rset);
+     FD_ZERO(&allset);
+     maxfd=lfd;
+     for(;;)
+     {
+         rset=allset;
+         int nready=select(maxfd+1,&rset,NULL,NULL,NULL);
+         if(nready<=0) continue;
+
+         if(FD_ISSET(lfd,&rset))
+         {
+             int cfd=accept(lfd,NULL,NULL);
+             for(i=0;i<FD_SETSIZE;i++)
+             {
+                 if(clients[i]==-1)
+                 {
+                     clients[i]=cfd;
+                     maxfd=maxfd>cfd?maxfd:cfd;
+                     break;
+                 }
+             }
+             if(i==FD_SETSIZE)
+             {
+                 printf("too many client\n");
+                 exit(0);
+             }
+             FD_SET(cfd,&allset);
+             if(--nready<=0) continue;
+         }
+
+         for(i=0;i<FD_SETSIZE;i++)
+         {
+             int fd=clients[i];
+             if(fd==-1) continue;
+             if(FD_ISSET(fd,&rset))
+             {
+                 char buf[1024]={};
+                 int r=read(fd,buf,1024);
+                 if(r<=0)
+                 {
+                     close(fd);
+                     clients[i]=-1;
+                    FD_CLR(fd,&allset);
+                 }else{
+                     write(fd,buf,r);
+                 }
+             }
+             if(--nready<=0) break;
+         }
+     }
+    
+}
